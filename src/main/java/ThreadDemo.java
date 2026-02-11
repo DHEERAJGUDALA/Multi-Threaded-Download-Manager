b30b2b262b9cc9f6e201e2ad55aac747acebab9b
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-11 14:37:59 +0530

    Download manager setup

---
+++ src/main/java/ThreadDemo.java
@@ -0,0 +1,7 @@
+package com.downloadmanager;
+public class ThreadDemo {
+    public static void main(String []args){
+        string name=Thread.currentThread().getName();
+        System.out.println("I am running on:"+ name);
+    }
+}
