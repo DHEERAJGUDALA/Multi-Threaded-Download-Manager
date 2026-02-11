2c05b0a85fe4363ec6d1794b059a9ab2236cc0a9
DHEERAJGUDALA <dheerajgudala1001lovesai200@gmail.com>
2026-02-11 17:05:17 +0530

    "Created RaceconditionDemo class"

---
+++ src/main/java/com/downloadmanager/RaceConditionDemo.java
@@ -0,0 +1,49 @@
+package com.downloadmanager;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class RaceConditionDemo {
+    public static void main(String[] args){
+        System.out.println("=== Race Condition Demo ===");
+        System.out.println();
+        long[] unsafeCounter={0};
+
+        AtomicLong safeCounter=new AtomicLong(0);
+         ExecutorService pool = Executors.newFixedThreadPool(10);
+
+         for(int i=0;i<10;i++) {
+             pool.submit(() -> {
+                 for (int j = 0; j < 100_000; j++) {
+                     unsafeCounter[0]++;
+                     safeCounter.incrementAndGet();
+                 }
+             });
+         }
+             pool.shutdown();
+             try {
+                 if(!pool.awaitTermination(30, TimeUnit.SECONDS)){
+                     System.out.println("Tasks didn't finish in time! Forcing Shutdown");
+                     pool.shutdownNow();
+                     if(!pool.awaitTermination(10,TimeUnit.SECONDS)){
+                         System.err.println("Pool didn't Terminate");
+                     }
+                 }
+             }catch(InterruptedException e){
+               pool.shutdownNow();
+               Thread.currentThread().interrupt();
+             }
+             System.out.println("Expected: 1,000,000");
+             System.out.println("Unsafe Counter"+unsafeCounter[0]);
+             System.out.println("Safe Counter:"+safeCounter);
+             System.out.println();
+
+             if(unsafeCounter[0]!=100_000){
+                 System.out.println("Unsafe counter Lost"+(100_000-unsafeCounter[0])+"increments!");
+                 System.out.println("This is a Race Conditon");
+             }
+             System.out.print("AtomicLong is correct!");
+         }
+    }
